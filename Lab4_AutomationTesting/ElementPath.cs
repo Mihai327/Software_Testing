af186a3d92910816edf44c19991cbc62dcdb00f6
Mihai327 <62377695+Mihai327@users.noreply.github.com>
2021-04-19 07:24:54 -0600

    Add files via upload

---
+++ Lab4_AutomationTesting/ElementPath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4_AutomationTesting
+{
+    public class ElementPath
+    {
+        //aliexpress
+        
+        public string aliexpressInputTextSearchField = "//input[@name='SearchText']";
+        public string aliexpressHeader = "//span[@class='logo-base']";
+
+    }
+}
